4985350c41f5ca2f8b1d02171ee979149ce70230
Recep  Eren <eren@C02YT245LVCF-652BA.local>
2021-06-22 09:28:40 -0400

    Game is finalized and works.

--- PythonPractices/Conway's Game of Life/Conway's Game of Life.py
+++ PythonPractices/Conway's Game of Life/Conway's Game of Life.py
@@ -22,16 +22,61 @@ for x in range(WIDTH):
 # print(nextCell[3]) # DELETE END OF THE CODE!!!! you can try and see next cell.
 
 while True: # Main Program Loop.
-    print('/n/n/n/n/n') # Separate each step with new lines.
+    print('\n\n\n\n') # Separate each step with new lines.
     # currentCell creates the game table
     # currentCell will be printed on screen, necessary calculation will be held on current Cell (Dead cell , live cell etc), 
     currentCells = copy.deepcopy(nextCell) # Deepcopy is technical issue. Generally it is used if a list consist another list. nextCell has X and Y coordinates. Each Y number is stored under X numbers' list. 
 
-    # Printing the currentCell on the screen
+    # Printing a specific cell (one cell)
     for y in range(HEIGHT): # It goes line by line( 1st row on X coordinate and 2nd row , 3rd row etc.  )
         for x in range(WIDTH):
             print(currentCells[x][y], end='') # currentCell [x][y] will find a specific one cell, located in currentCells [x][y] location. end allow the print line to continue on one line. 
         print() # It prints a new line at the end of the code.
+
+    # Calculate the next step's cell based on the current step cells:
+    for x in range(WIDTH):
+        for y in range (HEIGHT):
+            # Gete neightboring coordinates
+            # '%WIDTH' ensures leftCoord is always between 0 and WIDTH -1. Basically it provides to go out of the coordinates.
+            leftCoord = (x-1)%WIDTH
+            rightCoord = (x+1)%WIDTH
+            aboveCoord = (y-1)%HEIGHT
+            belowCoord = (y+1)%HEIGHT
+
+            # Calculate the number of living neighbors.
+            numNeighbors = 0
+
+            if currentCells[leftCoord][aboveCoord] == '#':
+                numNeighbors+=1 # Top-left neighbor is alive.
+            if currentCells[x][aboveCoord] == '#':
+                numNeighbors+=1 # Top neighbor is alive.    
+            if currentCells[rightCoord][aboveCoord] == '#':
+                numNeighbors+=1 #  Top-right neighbor is alive.
+            if currentCells[leftCoord][y] == '#':
+                numNeighbors+=1 #  Left neigbor neighbor is alive.
+            if currentCells[rightCoord][y] == '#':
+                numNeighbors+=1 # Right neighbor is alive.
+            if currentCells[leftCoord][belowCoord] == '#':
+                numNeighbors+=1 # Bottom-left neighbor is alive.
+            if currentCells[x][belowCoord] == '#':
+                numNeighbors+=1 #  Bottom neighbor is alive.
+            if currentCells[rightCoord][belowCoord] == '#':
+                numNeighbors+=1 #  Bottom-right neighbor is alive.
+
+            # Evaluate the conditions according to number of dead and alive cells.
+
+            if currentCells[x][y] == '#' and (numNeighbors == 2 or numNeighbors ==3):
+                # 2 or 3 living cell neighbors will stay alive.
+                nextCell[x][y]=='#'
+            elif currentCells [x][y] == ' ' and numNeighbors ==3:
+                # Dead cell with a 3 neighbors will become alive.
+                nextCell[x][y] == '#'
+            else:
+                # Everything else dies or stays dead.
+                nextCell[x][y] == ' '
+        time.sleep(1) # Reduce flickering by pausing 1 second.
+
+
 
 
 
