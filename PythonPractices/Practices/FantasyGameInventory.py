92df5f87eb6581e074518c50fbb8d1c2dddbe3ee
Recep  Eren <eren@C02YT245LVCF-652BA.local>
2021-07-14 07:06:04 -0400

    Trying to update a dictionary from a given List

--- PythonPractices/Practices/FantasyGameInventory.py
+++ PythonPractices/Practices/FantasyGameInventory.py
@@ -1,14 +1,31 @@
-stuff = {'rope':7, 'torch':2,'gold coin':42,'dagger':1,'arrow':12}
+# stuff = {'rope':7, 'torch':2,'gold coin':42,'dagger':1,'arrow':12}
 
-def displayInventory(inventory):
-    print("Inventory: ")
+# def displayInventory(inventory):
+#     print("Inventory: ")
+#     item_total = 0
+
+#     for k,v in inventory.items():
+#         item_total = item_total + v
+#         print(f'{v} {k}' )
+#     print("Total Number of items: " + str(item_total))
+
+
+# displayInventory(stuff)
+
+'''Write a function that update inventory from a list of loots.'''
+
+def addToInventory (inventory, addedItems):
+    print("Inventory:")
+    print('----------')
     item_total = 0
 
     for k,v in inventory.items():
-        item_total = item_total + v
-        print(f'{v} {k}' )
-    print("Total Number of items: " + str(item_total))
+        item_total = item_total + v 
+        print(f'{k} {v}')
+    #Write Code here
 
+    # We need to Iterate over list and update the dictionary according to the list. HOW???!!!
 
-displayInventory(stuff)
-
+inv = {"gold coin":42, 'rope':1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+addToInventory(inv,dragonLoot)
